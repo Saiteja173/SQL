bc8a65153303aecbd9b81ce839a0a052e77bb07d
Saiteja173 <68246759+Saiteja173@users.noreply.github.com>
2020-08-19 23:23:07 +0530

    Update 1584480990_SQLTasks_Tier_2.sql

--- 1584480990_SQLTasks_Tier_2.sql
+++ 1584480990_SQLTasks_Tier_2.sql
@@ -63,9 +63,9 @@ FROM `Facilities`
 
 /* Q6: You'd like to get the first and last name of the last member(s)
 who signed up. Try not to use the LIMIT clause for your solution. */
-SELECT surname,firstname,joindate
-FROM `Members`
-ORDER BY joindate DESC
+
+SELECT surname, firstname, date( joindate )
+FROM `Members` where date(joindate) in(select max(date(joindate)) from Members)
 
 /* Q7: Produce a list of all members who have used a tennis court.
 Include in your output the name of the court, and the name of the member
